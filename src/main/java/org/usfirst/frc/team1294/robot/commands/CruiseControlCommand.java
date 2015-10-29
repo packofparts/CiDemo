0c47ea58b28283dab37cdf0c52a63ddc3015ce03
Austin Jenchi <timtim17@users.noreply.github.com>
2015-10-29 08:49:00 -0700

    Remove idea template

--- src/main/java/org/usfirst/frc/team1294/robot/commands/CruiseControlCommand.java
+++ src/main/java/org/usfirst/frc/team1294/robot/commands/CruiseControlCommand.java
@@ -1,7 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package org.usfirst.frc.team1294.robot.commands;
 
 import edu.wpi.first.wpilibj.command.Command;
