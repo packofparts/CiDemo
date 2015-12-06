49a4523ebffd040a1e9a436ce3bcd5e3068b8a7a
John R Lewis <jxlewis@godaddy.com>
2015-12-05 21:06:02 -0800

    Removed ReversedControls command

--- src/main/java/org/usfirst/frc/team1294/robot/OI.java
+++ src/main/java/org/usfirst/frc/team1294/robot/OI.java
@@ -1,7 +1,6 @@
 package org.usfirst.frc.team1294.robot;
 
 import org.usfirst.frc.team1294.robot.commands.DriveDistance;
-import org.usfirst.frc.team1294.robot.commands.ReversedControls;
 
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.buttons.Button;
