893b348f7f5292f89a91ed5b2604cd91eb1e9072
Austin Jenchi <timtim17@users.noreply.github.com>
2015-12-31 18:07:50 -0800

    Remove uneeded import

--- src/main/java/org/usfirst/frc/team1294/robot/Robot.java
+++ src/main/java/org/usfirst/frc/team1294/robot/Robot.java
@@ -1,7 +1,6 @@
 
 package org.usfirst.frc.team1294.robot;
 
-import edu.wpi.first.wpilibj.CameraServer;
 import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.Scheduler;
